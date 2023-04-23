264fd124a48828c2b4d47ea241bd65af06d10064
neerajfull <neeraj.manoj@anywhere.co>
2023-04-23 15:26:26 +0530

    pushing final code to github

---
+++ models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: [true, "User already exist"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    blogs: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Blog"
+    }]
+}, { timestamps: true })
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
---
+++ routes/blogRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { getAllBlogs, createBlogController, updateBlogController, deleteBlogController, getBlogByIdController, userBlogController } = require("../controllers/blogControllers");
+const { checkUser, isLogin } = require("../middlewares/authMiddleware");
+const router = express.Router();
+
+//get all blogs
+router.get("/all-blogs", checkUser, getAllBlogs);
+
+//create blogs
+router.post("/create-blog/:userId", checkUser, createBlogController);
+
+//update blog
+router.put("/update-blog/:blogId", checkUser, updateBlogController);
+
+//delete blog
+router.delete("/delete-blog/:blogId", checkUser, deleteBlogController);
+
+//get single Blog
+router.get("/get-blog/:blogId", checkUser, getBlogByIdController);
+
+//get user blog
+router.get("/user-blog/:userId", checkUser, userBlogController);
+
+module.exports = router;
